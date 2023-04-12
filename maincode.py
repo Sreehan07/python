51de14a63524338ce7e749b377be7e2606cbea4a
Sreehan07 <82800385+Sreehan07@users.noreply.github.com>
2023-04-13 02:58:27 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -10,12 +10,21 @@ now = datetime.now()
 
 dt_string = now.strftime("%H_%M_%S")
 print("date and time =", dt_string)
-dt_string="network_"+dt_string+".txt"
-
+dt_string=dt_string+".txt"
+#network file name 
+var="network_"+dt_string
+#system info file name
+var2="system_"+dt_string
+#memory usage fname
+var3="memory_"+dt_string
+#events log fname
+var4="eventlogs_"+dt_string
+#process dump
+var1="pinfo_"+dt_string
 # # traverse the software list
 Data = subprocess.check_output(['wmic', 'process', 'list', 'brief'])
 a = str(Data)
-f = open(dt_string, "w")
+f = open(var1, "w")
 
 try:
     for i in range(len(a)):
@@ -25,7 +34,7 @@ try:
 except IndexError as e:
  	print("All Done")
 
-"""network_information"""
+"""---------------------------------------network_information---------------------------------------------------"""
 def get_size(bytes, suffix="B"):
     """
     Scale bytes to its proper format
@@ -40,22 +49,23 @@ def get_size(bytes, suffix="B"):
         bytes /= factor
 
 # Network information
+f1=open(var,"w")
 print("="*40, "Network Information", "="*40)
 # get all network interfaces (virtual and physical)
 if_addrs = psutil.net_if_addrs()
 for interface_name, interface_addresses in if_addrs.items():
     for address in interface_addresses:
         
-        print(f"=== Interface: {interface_name} ===")
+        f1.write(f"=== Interface: {interface_name} ===\n")
         if str(address.family) == 'AddressFamily.AF_INET':
             str2=(f"  IP Address: {address.address}\n")+(f"  Netmask: {address.netmask}\n")+(f"  Broadcast IP: {address.broadcast}\n")
-            f.write(str2)
+            f1.write(str2)
         elif str(address.family) == 'AddressFamily.AF_PACKET':
             str2=(f"  MAC Address: {address.address}\n")+(f"  Netmask: {address.netmask}\n")+(f"  Broadcast MAC: {address.broadcast}\n")
-            f.write(str2)
+            f1.write(str2)
 # get IO statistics since boot
 net_io = psutil.net_io_counters()
 str2=(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}\n")
-f.write(str2)
+f1.write(str2)
 str2=(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
-f.write(str2)
+f1.write(str2)
